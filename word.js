4ffad2478f2900968222009683d372a88268fcd5
Ingrid Eddeny Valdez <101751922+blusheddeny@users.noreply.github.com>
2022-10-01 18:01:15 -0500

    add comments
    detailed steps to solve the challenge

--- word.js
+++ word.js
@@ -1,14 +1,23 @@
 function pigIt(str){
+  //convert string to array
+  //add map method to iterate through words
      var res = Array.from(str.split(' ')).map((e) => {
-      var move = Array.from(e)
+      //create array from word
+       var move = Array.from(e)
+       //conditional to ignore special characters
       if(e != '!' && e != '?'){
-      var end = move[0] + "ay"
+        //ending of each word
+        var end = move[0] + "ay"
+        //remove letter in index 0
       move.shift()
+        //add the ending to each word
       move.push(end)
       }
+       //join words without commas
       return move.join('')
     
 
   })
+     //resultant string
   return res.join(' ')
 }
